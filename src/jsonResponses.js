75085cef86cd5a185501089ddb4b2940dbae9db5
Nick Lang <nll5171@rit.edu>
2024-09-29 16:30:14 -0400

    Form now processes post request and adds/updates users.

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -11,7 +11,7 @@ const respondJSON = (request, response, status, object) => {
   });
 
   // HEAD requests don't get a body, just metadata
-  if (request.method !== 'HEAD' && status !== 204) response.write(content); 
+  if (request.method !== 'HEAD' && status !== 204) response.write(content);
 
   response.end();
 };
@@ -26,11 +26,42 @@ const getUsers = (request, response) => {
 };
 
 // Adds new user if they don't exist, updates age otherwise
-const updateUser = (request, response) => {
-    const newUser = {
+const addUser = (request, response) => {
+  const responseJSON = {
+    message: 'Name and age are both required.',
+  };
 
+  // Get name and age into object from request body
+  const { name, age } = request.body;
+
+  // Make sure we have both fields, else it's a bad request
+  if (!name || !age) {
+    response.id = 'addUserMissingParams';
+    return respondJSON(request, response, 400, responseJSON);
+  }
+
+  // If params exist, assume updating user by default
+  let responseCode = 204;
+
+  // If user doesn't yet exist, add user with 201 for created
+  if (!users[name]) {
+    responseCode = 201;
+    users[name] = {
+      name,
     };
-}
+  }
+
+  users[name].age = age;
+
+  // Send back message that user was created
+  if (responseCode === 201) {
+    responseJSON.message = 'Created successfully';
+    return respondJSON(request, response, responseCode, responseJSON);
+  }
+
+  // 204 shouldn't include response body. Sending back empty one
+  return respondJSON(request, response, responseCode, {});
+};
 
 // Page doesn't exist, returns 404
 const notFound = (request, response) => {
@@ -44,5 +75,6 @@ const notFound = (request, response) => {
 
 module.exports = {
   getUsers,
+  addUser,
   notFound,
 };
--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 const http = require('http');
+// querystring module for parsing querystrings from url
+const query = require('querystring');
 // pull in our custom files
 const htmlHandler = require('./htmlResponses.js');
 const jsonHandler = require('./jsonResponses.js');
@@ -7,60 +9,58 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 // struct that removes the need for lots of switch statements
 const urlStruct = {
-    '/': htmlHandler.getIndex,
-    '/style.css': htmlHandler.getCSS,
-    '/getUsers': jsonHandler.getUsers,
-    notFound: jsonHandler.notFound,
+  '/': htmlHandler.getIndex,
+  '/style.css': htmlHandler.getCSS,
+  '/getUsers': jsonHandler.getUsers,
+  notFound: jsonHandler.notFound,
 };
 
 // Handle requests that are sent in chunks
 const parseBody = (request, response, handler) => {
-    const body = [];
+  const body = [];
 
-    // If anything goes wrong, send badRequest back
-    request.on('error', (err) => {
-        console.dir(err);
-        response.statusCode = 400;
-        response.end();
-    });
+  // If anything goes wrong, send badRequest back
+  request.on('error', (err) => {
+    console.dir(err);
+    response.statusCode = 400;
+    response.end();
+  });
 
-    // Add chunk to the data until complete
-    request.on('data', (chunk) => {
-        body.push(chunk);
-    });
+  // Add chunk to the data until complete
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  });
 
-    // Combine data into url and add it to request body
-    // Call appropriate function for request afterwards
-    request.on('end', () => {
-        const bodyString = Buffer.concat(body).toString();
-        request.body = query.parse(bodyString);
+  // Combine data into url and add it to request body
+  // Call appropriate function for request afterwards
+  request.on('end', () => {
+    const bodyString = Buffer.concat(body).toString();
+    request.body = query.parse(bodyString);
 
-        handler(request, response);
-    });
+    handler(request, response);
+  });
 };
 
 // Handle post request stuff
 const handlePost = (request, response, parsedUrl) => {
-    if (parsedUrl.pathname === '/addUser') {
-        parseBody(request, response, jsonHandler.addUser);
-    }
+  if (parsedUrl.pathname === '/addUser') {
+    parseBody(request, response, jsonHandler.addUser);
+  }
 };
 
 const onRequest = (request, response) => {
-    // parse url into individual parts
-    // returns an object of url parts by name
-    const protocol = request.connection.encrypted ? 'https' : 'http';
-    const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
+  // parse url into individual parts
+  // returns an object of url parts by name
+  const protocol = request.connection.encrypted ? 'https' : 'http';
+  const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
 
-    if (request.method === 'POST') handlePost(request, response, parsedUrl);
+  if (request.method === 'POST') return handlePost(request, response, parsedUrl);
 
-    else if (urlStruct[parsedUrl.pathname]) {
-        return urlStruct[parsedUrl.pathname](request, response);
-    }
-
-    return urlStruct.notFound(request, response);
+  if (urlStruct[parsedUrl.pathname]) {
+    return urlStruct[parsedUrl.pathname](request, response);
+  } return urlStruct.notFound(request, response);
 };
 
 http.createServer(onRequest).listen(port, () => {
-    console.log(`Listening on 127.0.0.1: ${port}`);
+  console.log(`Listening on 127.0.0.1: ${port}`);
 });
